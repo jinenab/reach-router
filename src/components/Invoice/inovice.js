bc70cd89bec5e26e21103f9ff8a636f1e38ae9a2
Jinen Abdelhak <jinenab123@gmail.com>
2019-09-14 16:14:18 +0100

    add a link to the inovices page when no pages added

--- src/components/Invoice/inovice.js
+++ src/components/Invoice/inovice.js
@@ -1,17 +1,19 @@
 import React from "react"
-import {Match} from "@reach/router"
+import {Match,Link} from "@reach/router"
 function Inovice(){
     return(<div>
         <Match path="/inovice/:id">
 {
     props  =>
+    props.match.id===null?
+
       ( <div>
     
     This is Inovice page number : {props.match.id}
     <br></br>
 
           </div>) 
-    
+    : (<div>You didn't add a new Inovice page please return to <Link to="/inovices">Inovices 's page</Link>  and add a new one</div>)
     }  
 
         </Match>

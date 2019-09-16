25d4c13cb0bfc07321d01a2f6665996640cab228
Jinen Abdelhak <jinenab123@gmail.com>
2019-09-16 21:50:08 +0100

    start redux

--- src/components/Vanilla/vanilla.js
+++ src/components/Vanilla/vanilla.js
@@ -1,9 +1,15 @@
-import React from "react"
-function Vanilla(){
-    return(<div>
-Vanilla
- 
-
-    </div>)
+import React,{Component} from "react"
+class Vanilla extends Component{
+    state={ Counter:0,
+    }
+    render()
+    {
+   return(<div>
+Vanilla-Counter
+ <br></br>
+ <button onClick={()=>{}}>+</button>
+ <button>-</button>
+ <button>increment if odd</button>
+ </div>)}
 }
 export default Vanilla

544354bba8cb284ea615bb5ede1f7df3af5ad636
Jinen Abdelhak <jinenab123@gmail.com>
2019-09-16 23:20:27 +0100

    counter and redux

--- src/components/Vanilla/vanilla.js
+++ src/components/Vanilla/vanilla.js
@@ -1,14 +1,21 @@
 import React,{Component} from "react"
+import { createStore } from 'redux'
+import reducers from "../../reducers"
+import {INC} from "../../actions"
 class Vanilla extends Component{
     state={ Counter:0,
     }
+
+
     render()
-    {
+    {    const store = createStore(reducers)
+
    return(<div>
 <h1>Vanilla-Counter : {this.state.Counter}
 </h1> <br></br>
 
- <button onClick={()=>{}}>+</button>
+ <button onClick={()=>{console.log(store.getState())
+     store.dispatch(INC())}}>+</button>
  <button>-</button>
  <button>increment if odd</button>
  </div>)}
--- src/reducers/vanilla.js
+++ src/reducers/vanilla.js
@@ -1,11 +1,11 @@
 import {INCREMENT} from "../actions/types"
 const initState={
-    counter:0
+    Counter:0
 }
  const vanilla=(state=initState,action)=>{
 switch(action.type){
     case INCREMENT:
-        return {counter:state.Counter+1}
+        return {Counter:state.Counter+1}
     default:
         return state
 }
